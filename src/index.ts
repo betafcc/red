028179eba8baa2fceee863354bcc70ce504bfb53
Beta Faccion <betafcc@gmail.com>
2020-05-21 18:56:44 -0200

    small renaming; starts doc strings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export class Red<S, A extends Action<string, any[]>> {
+export class Red<S, A extends ActionType<string, any[]>> {
   actions: ActionCreators<A>
 
   constructor(readonly initial: S, readonly handlers: Handlers<S, A>) {
@@ -24,19 +24,29 @@ export class Red<S, A extends Action<string, any[]>> {
       ...handlers,
     })
 
-  withActions = <B extends Action<string, any[]>>(
+  withActions = <B extends ActionType<string, any[]>>(
     handlers: {
       [K in B['type']]: (s: S, ...p: Extract<B, { type: K }>['payload']) => Partial<S>
     }
   ) => this.handle(handlers) as Red<S, A | B>
 
+  /**
+   * Defines the type and initial state
+   * @example
+   * const app = red.withState({ count: 0, input: '' })
+   * // app: Red<{count: number, input: string}, never>
+   *
+   * // or with state type defined explicitly
+   * type State = { count: number, input: string }
+   * const app = red.withState<State>({count: 0, input: ''})
+   */
   withState = <R>(initial: R) =>
     new Red({ ...this.initial, ...initial }, this.handlers as Handlers<S & R, A>)
 
   combine = <R extends Record<string, Red<any, any>>>(reds: R) =>
     this.merge(combine(reds))
 
-  merge = <R, B extends Action<string, any>>(other: Red<R, B>) =>
+  merge = <R, B extends ActionType<string, any>>(other: Red<R, B>) =>
     this.withState(other.initial).handle(other.handlers as Handlers<S & R, B>) as Red<
       S & R,
       A | B
@@ -79,7 +89,10 @@ export const combine = <R extends Record<string, Red<any, any>>>(reds: R) =>
     new Red<{ [K in keyof R]: StateOf<R[K]> }, ActionOf<R[keyof R]>>({} as any, {} as any)
   )
 
-export type Action<K, A> = { type: K; payload: A }
+export type ActionType<K extends string, A extends Array<unknown>> = {
+  type: K
+  payload: A
+}
 
 export type ActionOf<R> = R extends { reducer: (s: any, a: infer A) => any } ? A : never
 
@@ -87,21 +100,21 @@ export type StateOf<R> = R extends { initial: infer S } ? S : never
 
 // {a: [t]; b: [u, k]} -> {type: 'a'; payload: [t]} | {type: 'b'; payload: [u, k]}
 export type ActionFromMapping<AP extends Record<string, any[]>> = {
-  [K in Extract<keyof AP, string>]: Action<K, AP[K]>
+  [K in Extract<keyof AP, string>]: ActionType<K, AP[K]>
 }[Extract<keyof AP, string>]
 
 export type HandlersFromMapping<S, AP extends Record<string, any[]>> = {
   [K in keyof AP]: (s: S, ...p: AP[K]) => Partial<S>
 }
 
-export type Handlers<S, A extends Action<any, any>> = {
+export type Handlers<S, A extends ActionType<any, any>> = {
   [K in A['type']]: (s: S, ...p: Extract<A, { type: K }>['payload']) => Partial<S>
 }
 
-export type ActionCreators<A extends Action<string, any>> = {
+export type ActionCreators<A extends ActionType<string, any>> = {
   [K in A['type']]: (...p: Extract<A, { type: K }>['payload']) => Extract<A, { type: K }>
 }
 
-export type Dispatchers<A extends Action<string, any>> = {
+export type Dispatchers<A extends ActionType<string, any>> = {
   [K in A['type']]: (...p: Extract<A, { type: K }>['payload']) => void
 }
