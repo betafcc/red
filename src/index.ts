a743acddb95d5ad62d519d017a51ac02a23c2060
Beta Faccion <betafcc@gmail.com>
2020-05-22 06:42:03 -0200

    adds doc strings

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,43 @@ export class Red<S, A extends ActionType<string, any[]>> {
       : {}),
   })
 
+  /**
+   * Adds handlers to the reducer.
+   * The name of the handler will be 'type' of the action,
+   * the arguments will be the payload.
+   * 
+   * The return value of a handler must be the partial of State.
+   * @example
+   * const app = red
+   *   .withState({ count: 0, input: '' })
+   *   .handle({
+   *     // will infer action of shape: { type: 'setInput', payload: [string] }
+   *     setInput(state, msg: string) { return { input: msg } },
+   *     // will infer action of shape: { type: 'increment', payload: [] }
+   *     increment(state) { return { count: state.count + 1 } },
+   *   })
+   */
   handle = <AP extends Record<string, any[]>>(handlers: HandlersFromMapping<S, AP>) =>
     new Red<S, A | ActionFromMapping<AP>>(this.initial, {
       ...this.handlers,
       ...handlers,
     })
 
+  /**
+   * Same as `red.handle`, but instead of type inference on the handlers,
+   * the action types must be explicitly defined and will be enforced in the implementation:
+   * 
+   * @example
+   * type Action =
+   *   | ActionType<'setInput', [{msg: string}]>
+   *   | ActionType<'increment'>
+   * 
+   * const app = red
+   *   .withState({ count: 0, input: '' })
+   *   .withActions<Action>({ // if some key is missing, will point
+   *     setInput
+   *   })
+   */
   withActions = <B extends ActionType<string, any[]>>(
     handlers: {
       [K in B['type']]: (s: S, ...p: Extract<B, { type: K }>['payload']) => Partial<S>
@@ -43,15 +74,76 @@ export class Red<S, A extends ActionType<string, any[]>> {
   withState = <R>(initial: R) =>
     new Red({ ...this.initial, ...initial }, this.handlers as Handlers<S & R, A>)
 
+  /**
+   * Similar to redux's `combineReducers` but for reds.
+   * Namespaces the state of each red under a key, and combine it's actions.
+   * It's easier to define independent sections of the app separetly, and combine then with namespaced state:
+   * @example
+   * const inputApp = red.withState({input: ''}).handle({setInput: (s, input: string) => ({input})})
+   * const counterApp = red.withState({count: 0}).handle({increment: s => ({count: s.count + 1})})
+   * 
+   * const app = red.combine({
+   *   ui: inputApp,
+   *   counter: counterApp
+   * })
+   * 
+   * // equivalent to:
+   * const app = red.withState({
+   *   ui: {input: ''},
+   *   counter: {count: 0}
+   * }).handle({
+   *   // note that you have to manually namespace the state
+   *   setInput: (s, input: string) => ({...s, ui: {input}}),
+   *   counter: s => ({...s, counter: {count: s.counter.count + 1}})
+   * })
+   */
   combine = <R extends Record<string, Red<any, any>>>(reds: R) =>
     this.merge(combine(reds))
 
+  /**
+   * Merges two reds together, it's the same as defining state and actions in sequence:
+   * @example
+   * const inputApp = red
+   *   .withState({input: ''})
+   *   .handle({setInput: (s, value: string) => ({input: value})})
+   * 
+   * const todoApp = red
+   *   .withState({todos: [] as Array<{id: number, done: boolean, msg: string}>})
+   *   .handle({add: (s, msg: string, id: number) => ({todos: [...s.todos, {id, msg, done: false}]}) })
+   * 
+   * const app = inputApp.merge(todoApp)
+   * // same as
+   * const app = red
+   *   .withState({input: ''})
+   *   .handle({setInput: (s, value: string) => ({input: value})})
+   *   .withState({todos: [] as Array<{id: number, done: boolean, msg: string}>})
+   *   .handle({add: (s, msg: string, id: number) => ({todos: [...s.todos, {id, msg, done: false}]}) })
+   */
   merge = <R, B extends ActionType<string, any>>(other: Red<R, B>) =>
     this.withState(other.initial).handle(other.handlers as Handlers<S & R, B>) as Red<
       S & R,
       A | B
     >
 
+  /**
+   * Wraps 'dispatch' function for each action creator
+   * @example
+   * const app = red.withState({count: 0}).handle({
+   *   increment: s => ({count: s.count + 1}),
+   *   decrement: s => ({count: s.count - 1})
+   * })
+   * 
+   * const App = () => {
+   *   const [state, dispatch] = useReducer(app.reducer, app.initial)
+   *   const {increment, decrement} = makeDispatchers(dispatch)
+   * 
+   *   return <>
+   *     {state.count}
+   *     <button onClick={increment}>+</button>
+   *     <button onClick={decrement}>-</button>
+   *   <>
+   * }
+   */
   makeDispatchers = (dispatch: (a: A) => void) =>
     Object.keys(this.actions).reduce(
       (acc, type) => ({
@@ -89,7 +181,7 @@ export const combine = <R extends Record<string, Red<any, any>>>(reds: R) =>
     new Red<{ [K in keyof R]: StateOf<R[K]> }, ActionOf<R[keyof R]>>({} as any, {} as any)
   )
 
-export type ActionType<K extends string, A extends Array<unknown>> = {
+export type ActionType<K extends string, A extends Array<unknown> = []> = {
   type: K
   payload: A
 }
